d9f9b61d70982d716bb795611714c7e2543c48ba
Alvaro Silva <alvaro.augustho@gmail.com>
2016-11-06 18:12:40 +0100

    Deleting test code

--- main.py
+++ main.py
@@ -7,16 +7,11 @@ def main():
     lt.attachToken(cabecalho['authenticationToken'])
     issues_collection = mongo.get_collection()
 
-    i = 0
     for issue in issues_collection.find({'tipoDocumento':'ISSUE_INTEGRACAO'}):
         if not issue['externalId']:
             issues.create_issue(lt, issues_collection, issue)
         if issue['toUpdate']:
             issues.update_issue(lt, issues_collection, issue)
-
-            i+=1
-            if i == 5:
-                break
 
 if __name__ == "__main__":
     main()
