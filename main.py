ab3fded39e407f2b0e16a605875aa5b8853325f8
Alvaro Silva <alvaro.augustho@gmail.com>
2016-11-16 13:40:24 +0100

    Try except blocks in main

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ from libs import mongo, lt_client, issues
 def main():
 
     lt = lt_client.get_lt_client()
-    cabecalho = mongo.get_cabecalho()
     issues_collection = mongo.get_collection()
 
     for issue in issues_collection.find({'tipoDocumento':'ISSUE_INTEGRACAO'}):
         if not issue['externalId']:
-            issues.create_issue(lt, issues_collection, issue)
+            try:
+                issues.create_issue(lt, issues_collection, issue)
+            except Exception as err:
+                print("Erro ao criar issue "+ issue['issuekey'] + ": ", err)
+
         if issue['toUpdate']:
-            issues.update_issue(lt, issues_collection, issue)
+            try:
+                issues.update_issue(lt, issues_collection, issue)
+            except Exception as err:
+                print("Erro ao atualizar issue "+ issue['issuekey'] + ": ", err)
 
 if __name__ == "__main__":
     main()
